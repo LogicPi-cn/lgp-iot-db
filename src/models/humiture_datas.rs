2a5804188f50dcccde6871b969303f22be28b973
Craftor <craftor@126.com>
2023-05-28 20:12:15 +0800

    update db

--- src/models/humiture_datas.rs
+++ src/models/humiture_datas.rs
@@ -1,6 +1,6 @@
-use chrono::{Local, NaiveDateTime};
+use chrono::{Duration, Local, NaiveDateTime};
 use diesel::{AsChangeset, Insertable, PgConnection, QueryDsl, Queryable, RunQueryDsl};
-use log::info;
+use log::{debug, error, info};
 use rand::Rng;
 use serde_derive::{Deserialize, Serialize};
 
@@ -55,6 +55,69 @@ impl NewHumitureData {
             temperature: r * (angle * 3.1415926 / 180.0).sin(),
             humidity: r * (angle * 3.1415926 / 180.0).cos(),
         }
+    }
+
+    // get some data from bytes
+    pub fn from_bytes(bytes: &[u8], n: i32) -> Vec<Self> {
+        let mut result = Vec::new();
+
+        // unpackage the data
+        if bytes.len() > 0 {
+            // compare the head
+            if bytes[0..2] == [0x5A, 0xA5] {
+                // check the length
+                let len = bytes[2] as usize;
+                if len == bytes.len() - 4 {
+                    let id = &bytes[3..11];
+                    let sn = &bytes[11..15];
+
+                    // get current time
+                    let fmt = "%Y-%m-%d %H:%M:%S";
+                    let naive = Local::now().format(fmt).to_string();
+                    let now = NaiveDateTime::parse_from_str(&naive, fmt).unwrap();
+
+                    for i in 0..n {
+                        let tt = u16::from_be_bytes([
+                            bytes[(23 + i * 2) as usize],
+                            bytes[(24 + i * 2) as usize],
+                        ]);
+                        let hh = u16::from_be_bytes([
+                            bytes[(23 + i * 2 + 2 * n) as usize],
+                            bytes[(24 + i * 2 + 2 * n) as usize],
+                        ]);
+                        let t = tt as f32 / 10.0;
+                        let h = hh as f32 / 10.0;
+
+                        // seperate the time(2h) into n slices
+                        let ts = now - Duration::minutes(((n - i) * (120 / n)).into());
+
+                        debug!("id={:?}", hex::encode(id));
+                        debug!("sn={:?}", hex::encode(sn));
+                        debug!("t={}, h={}", t, h);
+                        debug!("ts={}", ts);
+
+                        // add the vector
+                        result.push(NewHumitureData {
+                            sn: hex::encode(sn),
+                            ts,
+                            device_id: hex::encode(id),
+                            temperature: t,
+                            humidity: h,
+                        })
+                    }
+                } else {
+                    error!(
+                        "length error : real is {}, total len is {}",
+                        len,
+                        bytes.len()
+                    );
+                }
+            } else {
+                error!("bad header");
+            }
+        }
+
+        return result;
     }
 
     // get new data from bytes string
@@ -127,4 +190,19 @@ impl HumitureData {
             .expect("Error on Delete");
         Ok(num_deleted)
     }
+}
+
+#[test]
+fn test_from_bytes() {
+    use env_logger;
+    use hex;
+
+    env_logger::init();
+
+    let hex_string = "5aa546e85f0022005700aa6c6f6769bd0217051e0d1105010d010e010e001f0110011001ff011000ffffffffff010d02af02af02ae024f027b027a02ff028102ffffffffff02ae34006d";
+    let bytes = hex::decode(hex_string).unwrap();
+
+    let result = NewHumitureData::from_bytes(&bytes, 12);
+
+    assert_eq!(result.len(), 12);
 }
