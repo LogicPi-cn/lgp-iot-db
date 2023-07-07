d44116c876eb9c8d73684a3b4086190b3c7998f2
Craftor <craftor@126.com>
2023-07-07 13:59:06 +0800

    bug fix for example

--- examples/humiture.rs
+++ examples/humiture.rs
@@ -1,10 +1,10 @@
-use lgp_iot_db::models::humiture_data_v2::{init_tdengine, insert_humiture, HumitureData};
+use lgp_iot_db::models::humiture_data_v2::{init_tdengine_humiture, insert_humiture, HumitureData};
 use taos::*;
 use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let taos = init_tdengine().await?;
+    let taos = init_tdengine_humiture("taos://localhost:6030", "humiture_test").await?;
 
     let random_data = HumitureData::random();
     insert_humiture(random_data, &taos).await?;
