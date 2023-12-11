02e18c28bf9fce39cc45982c918ec4278dac1e60
Craftor <craftor@126.com>
2023-12-11 17:25:07 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use diesel::prelude::*;
-use diesel::r2d2::{self, ConnectionManager};
+// use diesel::prelude::*;
+// use diesel::r2d2::{self, ConnectionManager};
 
 pub mod errors;
 pub mod models;
-pub mod schema;
+// pub mod schema;
 
-pub type DbError = Box<dyn std::error::Error + Send + Sync>;
-pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+// pub type DbError = Box<dyn std::error::Error + Send + Sync>;
+// pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,4 +1,4 @@
 pub mod adxl_data_v2;
-pub mod adxl_datas;
+// pub mod adxl_datas;
 pub mod humiture_data_v2;
-pub mod humiture_datas;
+// pub mod humiture_datas;
